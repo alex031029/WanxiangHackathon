c7c61b297e61a0f5363203cecae24677dfec0812
wujj <wujj@cpchain.io>
2019-09-14 13:30:02 +0800

    query name fn

--- backend/server/did/models.py
+++ backend/server/did/models.py
@@ -4,7 +4,7 @@ from django.db import models
 # Create your models here.
 
 class Issuer(models.Model):
-    did = models.CharField(max_length=48)
+    did = models.CharField(max_length=48,primary_key=True)
     name = models.CharField(max_length=100)
     industry = models.CharField(max_length=100)
 
--- backend/server/did/views.py
+++ backend/server/did/views.py
@@ -1,13 +1,18 @@
 from django.http import HttpResponse, JsonResponse
 from django.shortcuts import render
+from django.db.models import Q
+from did.models import Issuer
+from django.core import serializers
 
 
 # Create your views here.
 
 def search(req):
-    name = req.GET.get('name')
-
-    return JsonResponse({})
+    name = req.GET.get('query_key')
+    issuers = Issuer.objects.filter(Q(name__contains=name) | Q(industry__contains=name))
+    print(issuers)
+    data = serializers.serialize('json', issuers)
+    return HttpResponse(data)
 
 
 def detail(req):
