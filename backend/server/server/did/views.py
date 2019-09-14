4657e7931d9642e7f1ec4a8cd0078c72d344b10b
wujj <wujj@cpchain.io>
2019-09-14 11:49:12 +0800

    add url

--- backend/server/server/did/views.py
+++ backend/server/server/did/views.py
@@ -1,3 +1,17 @@
+from django.http import HttpResponse, JsonResponse
 from django.shortcuts import render
 
+
 # Create your views here.
+
+def search(req):
+    k = req.GET.get('did')
+    # print('did is', k)
+    
+
+    return JsonResponse(k)
+
+
+def detail(req):
+    m = {"k": "v"}
+    return JsonResponse(m)
--- backend/server/server/urls.py
+++ backend/server/server/urls.py
@@ -15,7 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-
+from server.did import views
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('search/',views.search),
+    path('detail/',views.detail)
 ]
